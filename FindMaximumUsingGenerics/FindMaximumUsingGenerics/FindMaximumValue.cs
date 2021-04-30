7abff890369483124085e34c88ea667a2dbc9117
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-30 18:13:06 +0530

    [Sanju] ADD : Compare 3 strings and find maximum.

--- FindMaximumUsingGenerics/FindMaximumUsingGenerics/FindMaximumValue.cs
+++ FindMaximumUsingGenerics/FindMaximumUsingGenerics/FindMaximumValue.cs
@@ -39,5 +39,22 @@ namespace FindMaximumUsingGenerics
             }
             return firstValue;
         }
+
+        public string CompareThreeStrings(string firstString, string secondString, string thirdString)
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0)
+            {
+                return firstString;
+            }
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0)
+            {
+                return secondString;
+            }
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0)
+            {
+                return thirdString;
+            }
+            return firstString;
+        }
     }
 }
--- FindMaximumUsingGenerics/FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/FindMaximumUsingGenerics/Program.cs
@@ -10,6 +10,7 @@ namespace FindMaximumUsingGenerics
             FindMaximumValue findMax = new FindMaximumValue();
             Console.WriteLine(findMax.CompareThreeIntegers(200, 250, 450));
             Console.WriteLine(findMax.CompareThreeFloatValues(20.5f, 12.8f, 4.3f));
+            Console.WriteLine(findMax.CompareThreeStrings("Apple", "Orange", "Banana"));
 
         }
     }
