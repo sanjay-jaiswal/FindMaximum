ac8bf596fe88500f7e919b7c1757b62a5a819bd6
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-30 11:18:52 +0530

    [Sanju] ADD : Compare 3 integers and find maximum.

---
+++ FindMaximumUsingGenerics/FindMaximumUsingGenerics/FindMaximumValue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaximumUsingGenerics
+{
+    public class FindMaximumValue
+    {
+        public int CompareThreeIntegers(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+    }
+}
---
+++ FindMaximumUsingGenerics/FindMaximumUsingGenerics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FindMaximumUsingGenerics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============================== Welcome To Find Maximum Program ===========================");
+            FindMaximumValue findMax = new FindMaximumValue();
+            Console.WriteLine(findMax.CompareThreeIntegers(200, 250, 450));
+        }
+    }
+}
